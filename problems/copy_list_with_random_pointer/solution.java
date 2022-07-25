f1de78dcd4eca959a97bc337de5e49ee10ea6b25
Atif Siddiqui <51026543+Atifsid@users.noreply.github.com>
2022-07-25 15:23:20 +0000

    Sync LeetCode submission - Copy List with Random Pointer (java)

---
+++ problems/copy_list_with_random_pointer/solution.java
@@ -0,0 +1,52 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node iter = head, front = head;
+        
+        // Step 1: Insert opy nodes right after original nodes
+        while(iter != null){
+            front = iter.next; 
+            Node copy = new Node(iter.val);
+            iter.next = copy;
+            copy.next = front;
+            iter = front;
+        }
+        
+        // Step 2: Point the random pointers
+        iter = head;
+        while(iter != null){
+            if(iter.random != null)
+                iter.next.random = iter.random.next;
+            iter = iter.next.next;
+        }
+        
+        // Step 3: Get the original an deep copy linked list
+        iter = head;
+        Node copyHead = new Node(0);
+        Node copy = copyHead;
+        
+        while(iter != null){
+            front = iter.next.next;
+            copy.next = iter.next;
+            iter.next = front;
+            copy = copy.next;
+            iter = iter.next;
+        }
+        
+        return copyHead.next;
+    }
+}
