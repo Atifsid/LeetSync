0d6ae4ccd404e8f00ef1a01656c0cdda824f026b
Atif Siddiqui <51026543+Atifsid@users.noreply.github.com>
2022-08-09 18:02:54 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (java)

---
+++ problems/search_in_rotated_sorted_array/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        
+        while(low <= high){
+            int mid  = (low + high) / 2;
+            if(target == nums[mid]) return mid;
+            
+            // Check if left half is sorted
+            if(nums[low] <= nums[mid]){
+                // Check if element lies in the left half or not
+                if(target >= nums[low] && target <= nums[mid]) high = mid - 1;
+                else low = mid + 1;
+                
+            }
+            // right half is sorted
+            else{
+                // Check if element lies in the right half or not
+                if(target >= nums[mid] && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
