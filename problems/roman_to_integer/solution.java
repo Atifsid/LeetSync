8c96a6eefd33152315007c0563e7dac90597dac2
Atif Siddiqui <51026543+Atifsid@users.noreply.github.com>
2022-10-29 18:00:14 +0000

    Sync LeetCode submission - Roman to Integer (java)

--- problems/roman_to_integer/solution.java
+++ problems/roman_to_integer/solution.java
@@ -1,23 +1,24 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
         
-        int result = map.get(s.charAt(s.length() - 1));
+        int res = 0;
         
-        for(int i = s.length() - 2; i>=0; i--){
-            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
-                result -= map.get(s.charAt(i));
+        for(int i = 0; i<s.length(); i++){
+            if(i + 1 < s.length() && roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))){
+                res -= roman.get(s.charAt(i));
             }
-            else
-                result += map.get(s.charAt(i));
+            else{
+                res += roman.get(s.charAt(i));
+            }
         }
-        return result;
+        return res;
     }
 }
