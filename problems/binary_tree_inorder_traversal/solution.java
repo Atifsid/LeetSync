35c8f4490659ff7733102fd403a98714c1692d7d
Atif Siddiqui <51026543+Atifsid@users.noreply.github.com>
2022-11-07 13:07:06 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (java)

--- problems/binary_tree_inorder_traversal/solution.java
+++ problems/binary_tree_inorder_traversal/solution.java
@@ -13,21 +13,44 @@
  *     }
  * }
  */
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         return inorder(res, root);
+//     }
+    
+//     public List<Integer> inorder(List<Integer> res, TreeNode node){
+        
+//         if(node == null){
+//             return res;
+//         }
+//         inorder(res,node.left);
+//         res.add(node.val);
+//         inorder(res,node.right);
+//         return res;
+//     }
+// }
+
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> st = new Stack<>();
         List<Integer> res = new ArrayList<>();
-        inorder(res, root);
-        return res;
-    }
-    
-    public List<Integer> inorder(List<Integer> res, TreeNode node){
-        
-        if(node == null){
-            return res;
+        TreeNode node = root;
+        while(true){
+            if(node != null){
+                st.push(node);
+                node = node.left;
+            }
+            else{
+                if(st.isEmpty()) break;
+                
+                node = st.pop();
+                res.add(node.val);
+                node = node.right;
+            }
         }
-        inorder(res,node.left);
-        res.add(node.val);
-        inorder(res,node.right);
         return res;
     }
 }
