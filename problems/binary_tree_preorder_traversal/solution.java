0561dc64159fb25b68b880ab72a7c60ef6d7f3eb
Atif Siddiqui <51026543+Atifsid@users.noreply.github.com>
2022-11-16 13:17:28 +0000

    Sync LeetCode submission - Binary Tree Preorder Traversal (java)

--- problems/binary_tree_preorder_traversal/solution.java
+++ problems/binary_tree_preorder_traversal/solution.java
@@ -13,21 +13,43 @@
  *     }
  * }
  */
+
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         return preorder(res, root);   
+//     }
+    
+//     public List<Integer> preorder(List<Integer> res, TreeNode node){
+        
+//         if(node == null){
+//             return res;
+//         }
+//         res.add(node.val);
+//         preorder(res,node.left);
+//         preorder(res,node.right);
+//         return res;
+//     }
+// }
+
+
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        return preorder(res, root);
-        //return res;   
-    }
-    
-    public List<Integer> preorder(List<Integer> res, TreeNode node){
+        List<Integer> preorder = new ArrayList<>();
+        if(root == null) return preorder;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
         
-        if(node == null){
-            return res;
+        while(st.isEmpty() != true){
+            root = st.pop();
+            preorder.add(root.val);
+            if(root.right != null){
+                st.push(root.right);
+            }
+            if(root.left != null){
+                st.push(root.left);
+            }
         }
-        res.add(node.val);
-        preorder(res,node.left);
-        preorder(res,node.right);
-        return res;
+        return preorder;
     }
 }
