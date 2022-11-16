e67cb7984475f5aa1976e8d2c5cbcfdd8f5e85d9
Atif Siddiqui <51026543+Atifsid@users.noreply.github.com>
2022-11-16 14:01:30 +0000

    Sync LeetCode submission - Binary Tree Postorder Traversal (java)

--- problems/binary_tree_postorder_traversal/solution.java
+++ problems/binary_tree_postorder_traversal/solution.java
@@ -15,17 +15,41 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        return postOrder(res, root);
-    }
-    
-    public List<Integer> postOrder(List<Integer> res, TreeNode node){
-        if(node == null) return res;
-        
-        postOrder(res, node.left);
-        postOrder(res, node.right);
-        res.add(node.val);
-        
-        return res;
+        ArrayList<Integer> postorder = new ArrayList<>();
+        if(root == null) return postorder;
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        st1.push(root);
+        while(st1.isEmpty() != true){
+            root = st1.pop();
+            st2.push(root);
+            if(root.left != null){
+                st1.push(root.left);
+            }
+            if(root.right != null){
+                st1.push(root.right);
+            }
+        }
+        while(st2.isEmpty() != true){
+            postorder.add(st2.pop().val);
+        }
+        return postorder;
     }
 }
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         return postOrder(res, root);
+//     }
+    
+//     public List<Integer> postOrder(List<Integer> res, TreeNode node){
+//         if(node == null) return res;
+        
+//         postOrder(res, node.left);
+//         postOrder(res, node.right);
+//         res.add(node.val);
+        
+//         return res;
+//     }
+// }
