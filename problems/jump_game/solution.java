93055bff9036223fd1c9db885b0238559eb0f120
Atif Siddiqui <51026543+Atifsid@users.noreply.github.com>
2022-12-26 13:32:20 +0000

    Sync LeetCode submission - Jump Game (java)

--- problems/jump_game/solution.java
+++ problems/jump_game/solution.java
@@ -1,15 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
-        int reachable = 0;
-        
-        for(int i = 0; i < nums.length ; i++){
-            
-            if(reachable < i) return false;
-            
-            reachable = Math.max(reachable, nums[i] + i);
+        int r = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(r < i) return false;
+            r = Math.max(r, nums[i] + i);
         }
         return true;
-    
     }
 }
